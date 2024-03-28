40b8220afc05cc02ea95e7888c4a6d455ae5e4f0
rgusev <roman.gusev@brsmed.com>
2024-03-28 10:24:34 +0300

    add timer

--- app/src/main.c
+++ app/src/main.c
@@ -26,6 +26,10 @@ static char *fish = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
 
 int main(void)
 {
+    TIM_HandleTypeDef my_tim;
+    uint32_t ticks0, ticks1;
+    char *membuf1, *membuf2;
+    
     SystemClock_Config(CPU_CLK_160M);
     printf("enter main\r\n");
 
@@ -33,6 +37,31 @@ int main(void)
     _psram.Init.Mode = PSRAM_MODE_QSPI;
     _psram.Instance = PSRAM;
 
+    my_tim.Instance = TIM0;
+    my_tim.Init.Unit = TIM_UNIT_US;
+    my_tim.Init.AutoReload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+    my_tim.Init.Period = 1000000; //each us will increase timer
+    //__HAL_TIM_ENABLE(&my_tim);
+    HAL_TIM_Base_Init(&my_tim); //HAL_TIM_Base_Init
+    HAL_TIM_Base_Start(&my_tim);
+    /*mem2mem*/
+    membuf1 = malloc(0x10000);
+    membuf2 = malloc(0x10000);
+    if(membuf1 == NULL || membuf2 == NULL){
+        printf("malloc error!\n");
+        while(1);
+    }
+    uint32_t *ptimer = (uint32_t *)(TIM_BASE + 0x20);
+
+    ticks0 = *ptimer;
+    ticks0 = TIM->TIM0_CNT;
+    //ticks0 = HAL_GetTick();
+    memcpy(membuf1, membuf2, 0x10000);
+    ticks1 = *ptimer;
+    ticks1 = TIM->TIM0_CNT;
+    //ticks1 = HAL_GetTick();
+    printf("mem2mem @64k is %d %d us\n", ticks1, ticks0);
+    
     HAL_PSRAM_Init(&_psram);
 
     init_heap(); // PSRAM alloc init
@@ -40,14 +69,13 @@ int main(void)
     for(i=0; i<64; i++){
         q[i] = (char *)psalloc(strlen(fish)+1);
         if(!q){
-	  printf("psalloc error!\n");
-	  continue;
-	}
-	else{
-	    strcpy(q[i], fish);
+            printf("psalloc error!\n");
+            continue;
+        }
+        else{
+            strcpy(q[i], fish);
             heap_walk();
             printf("q[%d]=[%s]\n", i, q[i]);
-
         }
         HAL_Delay(1000);        // 1s delay
     }
